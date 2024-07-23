90a4f61a23c1590c9ce8737e437ff8416c65a34b
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-07-23 11:04:52 +0000

    Update Employee.sql

--- AdventureWorks2012/HumanResources/Tables/Employee.sql
+++ AdventureWorks2012/HumanResources/Tables/Employee.sql
@@ -2,7 +2,7 @@
     [BusinessEntityID]  INT                 NOT NULL,
     [NationalIDNumber]  NVARCHAR (15)       NOT NULL,
     [LoginID]           NVARCHAR (256)      NOT NULL,
-    [Personall]           NCHAR (1)           NOT NULL,
+    [Anil]           NCHAR (1)           NOT NULL,
     [OrganizationNode]  [sys].[hierarchyid] NULL,
     [OrganizationLevel] AS                  ([OrganizationNode].[GetLevel]()),
     [JobTitle]          NVARCHAR (50)       NOT NULL,
